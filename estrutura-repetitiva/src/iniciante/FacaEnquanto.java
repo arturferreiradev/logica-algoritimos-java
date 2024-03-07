566447421a29bc2c97c38389b8a4336d1b6016bb
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-03-07 16:57:58 -0300

    Exemplo usando do-while

--- estrutura-repetitiva/src/iniciante/FacaEnquanto.java
+++ estrutura-repetitiva/src/iniciante/FacaEnquanto.java
@@ -4,13 +4,18 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class FacaEnquanto {
-
+	/*
+	 * Fazer um programa para ler uma temperatura em Celsius e mostrar o equivalente
+	 * em Fahrenheit. Perguntar se o usuário deseja repetir (s/n). Caso o usuário
+	 * digite "s", repetir o programa.
+	 */
+	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		char resp;
-		
+
 		do {
 			System.out.print("Digite a temperatura em Celsius: ");
 			double C = sc.nextDouble();
@@ -19,7 +24,7 @@ public class FacaEnquanto {
 			System.out.print("Deseja repetir (s/n)? ");
 			resp = sc.next().charAt(0);
 		} while (resp != 'n');
-		
+
 		sc.close();
 
 	}
